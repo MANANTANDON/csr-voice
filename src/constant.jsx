54991fe1ee1b6c7863713d751869d2727644b92c
Manan Tandon <manantandon@Manans-MacBook-Air.local>
2025-08-03 20:54:22 +0530

    Header completed..

--- src/constant.jsx
+++ src/constant.jsx
@@ -3,4 +3,23 @@ export const CATMENU = [
   { name: "Governance", link: "/category/governance" },
   { name: "Sustainable World", link: "/category/sustainable-word" },
   { name: "Laws", link: "/category/laws" },
+  { name: "Sectors", link: "/category/sectors" },
+];
+
+export const SECTORS = [
+  { name: "Agri & Food", link: "/category/agri-food" },
+  { name: "Water", link: "/category/water" },
+  { name: "Health", link: "/category/health" },
+  { name: "Environment", link: "/category/environment" },
+  { name: "Education", link: "/category/education" },
+  { name: "Woman and Child", link: "/category/woman-and-child" },
+  { name: "Tech and Innovation", link: "/category/tech-and-innovation" },
+  { name: "Natural Disaster", link: "/category/natural-disaster" },
+];
+
+export const MOBILECAT = [
+  { name: "Home", link: "/" },
+  { name: "Governance", link: "/category/governance" },
+  { name: "Sustainable World", link: "/category/sustainable-word" },
+  { name: "Laws", link: "/category/laws" },
 ];
--- src/pages/category/[slug].jsx
+++ src/pages/category/[slug].jsx
@@ -1,27 +1,52 @@
 import { NewsCard } from "@/components/Cards/NewsCard";
 import { Layout } from "@/components/Layout/Layout";
 import { Container, Typography } from "@mui/material";
-import { Grid } from "@mui/system";
+import { Box, Grid } from "@mui/system";
 import axios from "axios";
 import React from "react";
 
 const Category = ({ posts }) => {
-  console.log(posts, "Manan Tandon");
+  console.log(posts.data.length);
   return (
     <>
       <Layout>
-        <Container maxWidth="xl">
-          <Typography fontSize="28px" sx={{ py: 2 }}>
-            {posts?.category?.name}
-          </Typography>
-          <Grid container sx={{ bgcolor: "#FFFFFF" }}>
-            {posts?.data?.map((item, key) => (
-              <Grid item size={{ xs: 12, md: 3 }} key={key}>
-                <NewsCard news={item} />
+        <Box
+          sx={{
+            my: { xs: 2, md: 5 },
+            height: posts.data.length <= 4 && "100vh",
+          }}
+        >
+          <Container maxWidth="xl">
+            <Box
+              sx={{
+                bgcolor: "#FFFFFF",
+                p: 2,
+                borderRadius: "7px",
+                border: "1.5px solid #e8e8e8",
+              }}
+            >
+              <Typography
+                fontSize={{ xs: "32px", md: "52px" }}
+                className="font-700"
+                sx={{
+                  color: "#080808",
+                  borderBottom: "3px solid #1877F2",
+                  pb: -2,
+                  mb: 2,
+                }}
+              >
+                {posts?.category?.name}
+              </Typography>
+              <Grid container rowGap={5}>
+                {posts?.data?.map((item, key) => (
+                  <Grid item size={{ xs: 12, md: 3 }} key={key}>
+                    <NewsCard news={item} />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-        </Container>
+            </Box>
+          </Container>
+        </Box>
       </Layout>
     </>
   );
