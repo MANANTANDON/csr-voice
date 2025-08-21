294805a1168b78506e1fbb5f305eb05556f793bc
Manan Tandon <manantandon@gmail.com>
2025-08-21 18:04:15 +0530

    next config resolved...

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["dev.csrvoice.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dev.csrvoice.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
